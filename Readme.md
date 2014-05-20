8933c42d63a4d654036d5dce489e10dda7a4e16c
TJ Holowaychuk <tj@vision-media.ca>
2014-05-20 15:12:52 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,47 @@
+# loggly-go
+
+  Loggly client for Go.
+
+## Installation
+
+    $ go get github.com/segmentio/loggly-go
+
+## Example
+
+```go
+package main
+
+import "github.com/segmentio/loggly-go"
+
+func main() {
+  client := loggly.New("8bad16f2-6c0e-4d90-944e-51238379f8d47")
+
+  client.Error("boom")
+
+  client.Info("connecting", loggly.Message{
+    "some": "details",
+    "here": 123,
+  }})
+}
+```
+
+## Options
+
+  By default the client will flush every __100__ messages _or_ every __5__ seconds. A `.timestamp` property is also provided per log, and a map of overridable properties is provided, but defaults to only `.hostname`.
+
+ - `.Debugging` (bool) enable debug logs
+ - `.BufferSize` (int) size of the buffer [100]
+ - `.FlushInterval` (time.Duration) flush interval [5 seconds]
+ - `.Token` (string) loggly api token
+ - `.Endpoint` (string) loggly api url
+ - `.Defaults` (loggly.Message) default properties
+ - `.Level` (loggly.Level) log level [loggly.Info]
+
+## Levels
+
+ Syslog level methods are provided, as well
+ as a base `.Log()` call.
+
+## License
+
+ MIT
---
+++ loggly.go
@@ -0,0 +1,303 @@
+package loggly
+
+//
+// dependencies
+//
+
+import . "encoding/json"
+import "io/ioutil"
+import "net/http"
+import "strings"
+import "bytes"
+import "time"
+import "log"
+import "os"
+
+//
+// Library version
+//
+
+const Version = "0.0.1"
+
+//
+// Default API end-point
+//
+
+const api = "http://logs-01.loggly.com/bulk/{token}/tag/bulk"
+
+//
+// Message type
+//
+
+type Message map[string]interface{}
+
+//
+// Newline delim
+//
+
+var nl = []byte{'\n'}
+
+//
+// Log levels
+//
+
+type Level int
+
+const (
+	Debug Level = iota
+	Info
+	Notice
+	Warning
+	Error
+	Critical
+	Alert
+	Emergency
+)
+
+//
+// Loggly client
+//
+
+type Client struct {
+	Level         Level
+	Debugging     bool
+	BufferSize    int
+	FlushInterval time.Duration
+	Endpoint      string
+	Token         string
+	buffer        [][]byte
+	Defaults      Message
+}
+
+//
+// Return a new Loggly client with the given token.
+//
+
+func New(token string) (c *Client) {
+	host, err := os.Hostname()
+
+	if err != nil {
+		log.Fatal("failed to get hostname")
+	}
+
+	defaults := Message{
+		"hostname": host,
+	}
+
+	defer func() {
+		go func() {
+			for {
+				time.Sleep(c.FlushInterval)
+				c.log("interval %v reached", c.FlushInterval)
+				go c.flush()
+			}
+		}()
+	}()
+
+	return &Client{
+		Level:         Info,
+		Debugging:     false,
+		BufferSize:    100,
+		FlushInterval: 5 * time.Second,
+		Token:         token,
+		Endpoint:      strings.Replace(api, "{token}", token, 1),
+		buffer:        make([][]byte, 0),
+		Defaults:      defaults,
+	}
+}
+
+//
+// Buffer a log message.
+//
+
+func (c *Client) Log(msg Message) error {
+	msg["timestamp"] = time.Now()
+	merge(msg, c.Defaults)
+
+	json, err := Marshal(msg)
+
+	if err != nil {
+		return err
+	}
+
+	c.buffer = append(c.buffer, json)
+
+	c.log("buffer (%d/%d) %v", len(c.buffer), c.BufferSize, msg)
+
+	if len(c.buffer) >= c.BufferSize {
+		go c.flush()
+	}
+
+	return nil
+}
+
+//
+// Debug log.
+//
+
+func (c *Client) Debug(t string, props ...Message) error {
+	if c.Level > Debug {
+		return nil
+	}
+	msg := Message{"level": "debug", "type": t}
+	merge(msg, props...)
+	return c.Log(msg)
+}
+
+//
+// Info log.
+//
+
+func (c *Client) Info(t string, props ...Message) error {
+	if c.Level > Info {
+		return nil
+	}
+	msg := Message{"level": "info", "type": t}
+	merge(msg, props...)
+	return c.Log(msg)
+}
+
+//
+// Notice log.
+//
+
+func (c *Client) Notice(t string, props ...Message) error {
+	if c.Level > Notice {
+		return nil
+	}
+	msg := Message{"level": "notice", "type": t}
+	merge(msg, props...)
+	return c.Log(msg)
+}
+
+//
+// Warning log.
+//
+
+func (c *Client) Warn(t string, props ...Message) error {
+	if c.Level > Warning {
+		return nil
+	}
+	msg := Message{"level": "warning", "type": t}
+	merge(msg, props...)
+	return c.Log(msg)
+}
+
+//
+// Error log.
+//
+
+func (c *Client) Error(t string, props ...Message) error {
+	if c.Level > Error {
+		return nil
+	}
+	msg := Message{"level": "error", "type": t}
+	merge(msg, props...)
+	return c.Log(msg)
+}
+
+//
+// Critical log.
+//
+
+func (c *Client) Critical(t string, props ...Message) error {
+	if c.Level > Critical {
+		return nil
+	}
+	msg := Message{"level": "critical", "type": t}
+	merge(msg, props...)
+	return c.Log(msg)
+}
+
+//
+// Alert log.
+//
+
+func (c *Client) Alert(t string, props ...Message) error {
+	if c.Level > Alert {
+		return nil
+	}
+	msg := Message{"level": "alert", "type": t}
+	merge(msg, props...)
+	return c.Log(msg)
+}
+
+//
+// Emergency log.
+//
+
+func (c *Client) Emergency(t string, props ...Message) error {
+	if c.Level > Emergency {
+		return nil
+	}
+	msg := Message{"level": "emergency", "type": t}
+	merge(msg, props...)
+	return c.Log(msg)
+}
+
+//
+// Log in debug mode.
+//
+
+func (c *Client) log(format string, v ...interface{}) {
+	if c.Debugging {
+		log.Printf(format, v...)
+	}
+}
+
+//
+// Merge others into a.
+//
+
+func merge(a Message, others ...Message) {
+	for _, msg := range others {
+		for k, v := range msg {
+			a[k] = v
+		}
+	}
+}
+
+//
+// Flush the buffered messages.
+//
+
+func (c *Client) flush() error {
+	if len(c.buffer) == 0 {
+		c.log("no messages to flush")
+		return nil
+	}
+
+	c.log("flushing %d messages", len(c.buffer))
+	body := bytes.Join(c.buffer, nl)
+
+	c.buffer = nil
+
+	client := &http.Client{}
+	c.log("POST %s with %d bytes", c.Endpoint, len(body))
+	req, err := http.NewRequest("POST", c.Endpoint, bytes.NewBuffer(body))
+
+	if err != nil {
+		c.log("error: %v", err)
+		return err
+	}
+
+	req.Header.Add("User-Agent", "loggly-go (version: "+Version+")")
+	req.Header.Add("Content-Type", "text/plain")
+	req.Header.Add("Content-Length", string(len(body)))
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		c.log("error: %v", err)
+		return err
+	}
+
+	c.log("%d response", res.StatusCode)
+
+	if res.StatusCode >= 400 {
+		resp, _ := ioutil.ReadAll(res.Body)
+		c.log("error: %s", string(resp))
+	}
+
+	return err
+}
