53c85cf0067837fd3ff9af0bf2f0a3121730ca8d
TJ Holowaychuk <tj@vision-media.ca>
2014-05-31 18:40:22 -0700

    add simple example

---
+++ examples/simple.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/segmentio/loggly-go"
+import "time"
+
+func main() {
+	client := loggly.New("8bad16f2-6c0e-4d90-944e-5668779f8d47")
+
+	for {
+		client.Info("connecting")
+
+		client.Critical("failed to connect", map[string]interface{}{
+			"host": "some-address-here",
+			"port": "some-port-here",
+		})
+
+		time.Sleep(50 * time.Millisecond)
+	}
+}
