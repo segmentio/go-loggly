99c38bdec4067e02660612fc7eaa8653f2ee3368
TJ Holowaychuk <tj@vision-media.ca>
2014-07-22 07:44:50 -0700

    add examples

---
+++ examples/string.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/segmentio/go-loggly"
+import "time"
+import "os"
+
+func main() {
+	client := loggly.New("8bad16f2-6c0e-4d90-944e-5668779f8d47")
+	client.Writer = os.Stderr
+
+	for {
+		client.SendString("testing: some stuff here")
+		time.Sleep(15 * time.Millisecond)
+	}
+}
